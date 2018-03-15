4a2dc43a2bc3bb28d818cab060a03a1f9584889e
Dwayne McMillan <dwaynem@dantonio.bz>
2018-03-15 16:48:56 +0000

    added controller to redirect page back to edit after user makes changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'profile/index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
   
   get 'welcome/index'
 

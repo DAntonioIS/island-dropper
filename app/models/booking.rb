dcd404298b72148bddcb0d017b5ff4b8abd9b9be
Dwayne McMillan <dwaynem@dantonio.bz>
2018-03-16 16:38:06 +0000

    start adding booking form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
 
 	# every booking has a pick and drop off detail
-	has_many :delivery_detail
-	accepts_nested_attributes_for :delivery_detail
+	has_many :delivery_details
+	accepts_nested_attributes_for :delivery_details
 
 	#every booking has one tracking detail
 	has_one :tracking_detail 
